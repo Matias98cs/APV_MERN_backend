62c22a702d6fec54693f1275ee6c5ecb5762f3bd
Matias98cs <matias98cs@gmail.com>
2022-10-25 20:19:08 -0300

    se instalo dotenv

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import conectarDB from './config/db.js';
+import dotenv from 'dotenv'
 
 const app = express()
+
+dotenv.config()
+conectarDB()
 
 app.use('/', (req, res) => {
     res.send('Hola Mundo')
 })
 
-app.listen(4000, () => {
-    console.log(`Servidor funcionando en el puerto 4000`)
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`)
 })
