50c396c55313c8c65ee2a25798fb17966a965146
Matias98cs <matias98cs@gmail.com>
2022-10-26 11:28:45 -0300

    se instalo bcrypt para hashear passwords

--- models/Veterinario.js
+++ models/Veterinario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt'
 import generarId from '../helpers/generarId.js'
 
 const veterinarioSchema = mongoose.Schema({
@@ -34,6 +35,21 @@ const veterinarioSchema = mongoose.Schema({
         type: Boolean,
         default: false
     }
+})
+
+//pre revisar docu de mongoose
+veterinarioSchema.pre('save', async function(next) {
+    // console.log(`Antes de almacenar`)
+    // console.log(this)
+
+    //para que un password que fue hasheado no lo vuevla a hashear
+    //lo ignora
+    if(!this.isModified('password')){
+        //el next nos dice que se vaya la siguiente middleware
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
 })
 
 const Veterinario = mongoose.model('Veterinario', veterinarioSchema);
