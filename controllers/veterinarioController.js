6a8c60a47ddc33e665b9d6cde3a2973e72bc1f9b
Matias98cs <matias98cs@gmail.com>
2022-10-26 11:16:01 -0300

    registro de usuarios con token

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,7 +1,25 @@
-const registrar =  (req, res) => {
-    res.json({
-        msg : 'Registrando usario'
-    })
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) => {
+    const {email} = req.body
+    try {
+
+        //revisar si esta duplicado el usuario
+        const existeUsuario = await Veterinario.findOne({email})
+
+        if(existeUsuario){
+            const error = new Error('Usario ya registrado')
+            return res.status(400).json({msg: error.message})
+        }
+        const veneterinario = new Veterinario(req.body)
+        const veterinarioGuardado = await veneterinario.save()
+        res.json({
+            msg : 'Registrando usuario',
+            veterinarioGuardado
+        })
+    } catch (error) {
+        console.log(error)
+    }
 };
 
 const perfil = (req, res) => {
@@ -10,7 +28,33 @@ const perfil = (req, res) => {
     })
 }
 
+const confirmar = async (req, res) => {
+    const {token} = req.params
+    // console.log(req.params.token)
+    const usuarioConfirmar = await Veterinario.findOne({token})
+    if(!usuarioConfirmar){
+        const error = new Error('Token no valido')
+        res.status(400).json({
+            msg: error.message
+        })
+    }
+    try {
+        usuarioConfirmar.token = null
+        usuarioConfirmar.confirmado = true
+        await usuarioConfirmar.save()
+        res.status(200).json({
+            msg: `Usuario confirmado correctamente`
+        })
+
+    } catch (error) {
+        console.log(object)
+    }
+    
+    
+}
+
 export {
     registrar,
-    perfil
+    perfil,
+    confirmar
 }
